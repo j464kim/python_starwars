9a18ec8bd9f6eb756792525dd0a92af0303c7c35
JunGwon <j464kim@uwaterloo.ca>
2018-02-16 00:35:30 -0500

    creates Player that moves on key press

--- main.py
+++ main.py
@@ -16,21 +16,60 @@ turtle.bgcolor("black")
 turtle.ht()
 turtle.setundobuffer(1)
 turtle.tracer(1)
+screen = turtle.Screen()
 
 
 class Ufo(turtle.Turtle):
     def __init__(self, ufo_shape, color, init_x, init_y):
-        turtle.Turtle.__init__(self, shape = ufo_shape)
-        self.speed(0) # animation speed in the range 0..10
-        self.penup()
+        turtle.Turtle.__init__(self, shape=ufo_shape)
+        self.speed(0)   # animation speed in the range 0..10
+        self.penup()    # start drawing
         self.color(color)
         self.fd(0)
         self.goto(init_x, init_y)
         self.speed = 1
 
+    def move(self):
+        self.fd(self.speed)
+
+    def turn_left(self):
+        self.lt(45)
+        print('turning left')
+
+    def turn_right(self):
+        self.rt(45)
+        print('turning right')
+
+    def accelerate(self):
+        self.speed += 1
+        print('faster')
+
+    def decelerate(self):
+        self.speed -= 1
+        print('slower')
+
+
+# Player inherits Ufo
+class Player(Ufo):
+    def __init__(self, player_shape, color, init_x, init_y):
+        Ufo.__init__(self, player_shape, color, init_x, init_y)
+        self.speed = 2
+        self.lives = 3
+
 
 # Create my Ufo
-player = Ufo('triangle', 'white', 0, 0)
+player = Player('triangle', 'white', 0, 0)
+
+# Keyboard bindings
+screen.onkey(player.turn_left, "Left")
+screen.onkey(player.turn_right, "Right")
+screen.onkey(player.accelerate, "Up")
+screen.onkey(player.decelerate, "Down")
+screen.listen()
+
+# Main Game Loop
+while True:
+    player.move()
 
 
 delay = raw_input("Enter to finish")
